9be27759640f2dcff397e33fb97765998d61a3fd
Ana Carolina <aanaakaarolina@gmail.com>
2026-06-18 11:10:18 -0300

    add novo sidebar e footer

---
+++ src/public/modulos/sidebar/sidebar.js
@@ -0,0 +1,21 @@
+fetch("modulos/sidebar/sidebar.html")
+    .then(response => response.text())
+    .then(data => {
+
+        document.getElementById("sidebar-container").innerHTML = data;
+
+        const paginaAtual =
+            window.location.pathname.split("/").pop();
+
+        const links =
+            document.querySelectorAll(".sidebar a");
+
+        links.forEach(link => {
+
+            if(link.getAttribute("href") === paginaAtual){
+                link.classList.add("active");
+            }
+
+        });
+
+    });
